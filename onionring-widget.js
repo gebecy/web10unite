591a686c4f7c1df0cbdf886c3008e070260c7d33
gebecy <bogebecy@gmail.com>
2026-02-03 23:57:32 +0900

    fix shadow dom test

--- onionring-widget.js
+++ onionring-widget.js
@@ -4,88 +4,183 @@
 
 // === ONIONRING-WIDGET ===
 //this file contains the code which builds the widget shown on each page in the ring. ctrl+f 'EDIT THIS' if you're looking to change the actual html of the widget
+var tag = document.getElementById(ringID);
 
-var tag = document.getElementById(ringID); //find the widget on the page
+const shadow = tag.attachShadow({ mode: "open" });
 
-thisSite = window.location.href; //get the url of the site we're currently on
+thisSite = window.location.href;
 thisIndex = null;
+
+/* ======================
+   ここは普通のJS処理
+====================== */
 
 // URL正規化
 function getDomain(url) {
   return normalizeUrl(url).split("/")[0];
 }
+
 try {
   var thisDomain = getDomain(thisSite);
   for (var i = 0; i < sites.length; i++) {
-    var siteDomain = getDomain(sites[i].url);
-    if (thisDomain === siteDomain) {
+    if (thisDomain === getDomain(sites[i].url)) {
       thisIndex = i;
       break;
     }
   }
-} catch (e) {
-  for (i = 0; i < sites.length; i++) {
-    if (thisSite.startsWith(sites[i].url)) {
-      //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
-      thisIndex = i;
-      break; //when we've found the site, we don't need to search any more, so stop the loop
-    }
-  }
-}
-console.log(thisIndex);
+} catch (e) {}
 
-function randomSite() {
-  otherSites = sites.slice(); //create a copy of the sites list
-  otherSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
-  randomIndex = Math.floor(Math.random() * otherSites.length);
-  location.href = otherSites[randomIndex].url;
-}
+let html = "";
 
-//if we didn't find the site in the list, the widget displays a warning instead
+/* ======================
+   HTMLを組み立てる
+====================== */
+
 if (thisIndex == null) {
-  tag.insertAdjacentHTML(
-    "afterbegin",
-    `
-<table>
-  <tr>
-    <td>Web1.0同盟登録中…</td>
-  </tr>
-</table>
-  `,
-  );
+  html = `
+  <table>
+    <tr><td>Web1.0同盟登録中…</td></tr>
+  </table>
+  `;
 } else {
-  //find the 'next' and 'previous' sites in the ring. this code looks complex
-  //because it's using a shorthand version of an if-else statement to make sure
-  //the first and last sites in the ring join together correctly
   previousIndex = thisIndex - 1 < 0 ? sites.length - 1 : thisIndex - 1;
   nextIndex = thisIndex + 1 >= sites.length ? 0 : thisIndex + 1;
 
-  indexText = "";
-  //if you've chosen to include an index, this builds the link to that
-  if (useIndex) {
-    indexText = `<a href='${indexPage}' target='_blank'>ﾘｽﾄ</a> `;
-  }
+  const indexText = useIndex
+    ? `<a href="${indexPage}" target="_blank">ﾘｽﾄ</a>`
+    : "";
 
-  randomText = "";
-  //if you've chosen to include a random button, this builds the link that does that
-  if (useRandom) {
-    randomText = `<a href='javascript:void(0)' onclick='randomSite()'>ﾗﾝﾀﾞﾑ</a>|`;
-  }
+  const randomText = useRandom ? `<a href="#" id="randomBtn">ﾗﾝﾀﾞﾑ</a>|` : "";
 
-  //this is the code that displays the widget - EDIT THIS if you want to change the structure
-  tag.insertAdjacentHTML(
-    "afterbegin",
-    `
+  html = `
   <table>
     <tr>
-      <td class='webring-prev'><a href='${sites[previousIndex].url}'>&lt;</a></td>
-      <td class='webring-info'><a href='https://gebecy.github.io/web10unite/index.html' class='ringName' target='_blank'>${ringName}</a></br>
-      <span class='webring-links'>
-        ${randomText}
-        ${indexText}</span></td>
-      <td class='webring-next'><a href='${sites[nextIndex].url}'>&gt;</a></td>
+      <td class="webring-prev"><a href="${sites[previousIndex].url}">&lt;</a></td>
+      <td class="webring-info">
+        <a href="https://gebecy.github.io/web10unite/index.html" class="ringName" target="_blank">${ringName}</a><br>
+        <span class="webring-links">
+          ${randomText}
+          ${indexText}
+        </span>
+      </td>
+      <td class="webring-next"><a href="${sites[nextIndex].url}">&gt;</a></td>
     </tr>
   </table>
-  `,
-  );
+  `;
+}
+
+shadow.innerHTML = `
+<style>
+/* onionring.js is made up of four files - onionring-widget.js, onionring-index.js, onionring-variables.js and onionring.css (this one!)
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (蒜) house, last updated 2020-10-24 */
+
+/* === ONIONRING.CSS === */
+/* this file affects the style of the widget. remember to replace all instances of #web1_0 with whatever value you have for ringID in the onionring-widget.js file. make sure it still has the # at the front, though! probably easiest to use find+replace to do it */
+
+#web1_0 {
+  display: inline-block;
+  margin: 0 auto;
+  padding: 3px; /* creates some space around the widget */
+}
+
+#web1_0 table {
+  display: inline-block; /* ← 横並びの核心 */
+  vertical-align: middle;
+
+  border-collapse: collapse;
+  border-spacing: 0;
+
+  font-family: "MS UI Gothic", "ＭＳ ゴシック", monospace;
+  font-size: 14px;
+
+  background: #c0c0c0;
+  border: 3px ridge #fff;
+  height: 45px;
+  margin: 2px;
+}
+#web1_0 .webring-prev a {
+  text-align: center;
+}
+#web1_0 .webring-next a {
+  text-align: center;
+}
+#web1_0 .webring-prev a,
+#web1_0 .webring-next a {
+  display: table-cell;
+  width: 26px;
+  height: 35px;
+  background: #e0e0e0;
+  border: 2px outset #fff;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+  vertical-align: middle;
+}
+#web1_0 .ringName {
+  font-size: 14px;
+  text-decoration: none;
+  color: #000;
+}
+#web1_0 .webring-prev a:active,
+#web1_0 .webring-next a:active {
+  border: 2px inset #fff;
+}
+
+#web1_0 .webring-info {
+  padding: 0 10px;
+  background: #dcdcdc;
+  line-height: 1.3;
+}
+
+#web1_0 .webring-links a {
+  color: blue;
+  text-decoration: none;
+  font-size: 11px;
+  font-weight: bold;
+  margin: 0 3px;
+}
+
+#web1_0 .webring-links a:hover {
+  color: red;
+}
+
+#web1_0 .webring-info {
+  letter-spacing: 0.5px;
+}
+#web1_0.dark table {
+  background: #111;
+  border: 2px solid #00ff00;
+  color: #00ff00;
+}
+
+#web1_0.dark .webring-info {
+  background: #000;
+}
+
+#web1_0.dark .webring-prev a,
+#web1_0.dark .webring-next a {
+  background: #001100;
+  border: 1px solid #00ff00;
+  color: #00ff00;
+}
+#web1_0.dark .webring-links a {
+  color: rgb(0, 255, 255);
+}
+#web1_0.dark .ringName {
+  color: #00ff00;
+}
+</style>
+
+${html}
+`;
+
+const btn = shadow.getElementById("randomBtn");
+if (btn) {
+  btn.addEventListener("click", () => {
+    const otherSites = sites.slice();
+    otherSites.splice(thisIndex, 1);
+    location.href =
+      otherSites[Math.floor(Math.random() * otherSites.length)].url;
+  });
 }
