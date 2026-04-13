404b0b99804c43db800b98a0d861a4333c568a26
gebecy <bogebecy@gmail.com>
2026-04-14 00:05:44 +0900

    add 1 site

--- onionring-variables.js
+++ onionring-variables.js
@@ -97,6 +97,12 @@ var sites = [
     nsfw: false,
     img: "https://gebecy.github.io/web10unite/img/banner-1-20040.png",
   },
+  {
+    name: "星屑べーかりー",
+    url: "https://johnnote.skr.jp/",
+    nsfw: false,
+    img: "https://johnnote.skr.jp/img/banner001.png",
+  },
 ];
 
 //the name of the ring
